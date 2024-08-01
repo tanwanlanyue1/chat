3345cf6eb96e1278377739327be3d33a71c6c2b0
wust <1726932926@qq.com>
2024-08-01 09:35:05 +0800

    st 增加绑定手机/邮箱，消息页

---
+++ lib/ui/mine/have_seen/have_seen_controller.dart
@@ -0,0 +1,7 @@
+import 'package:get/get.dart';
+
+import 'have_seen_state.dart';
+
+class HaveSeenController extends GetxController {
+  final HaveSeenState state = HaveSeenState();
+}
---
+++ lib/ui/mine/have_seen/have_seen_state.dart
@@ -0,0 +1,5 @@
+import 'package:get/get.dart';
+
+class HaveSeenState {
+  final vip = false.obs;
+}

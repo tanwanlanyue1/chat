8a1c85f49fdf18e075d579cf9104c95d9957ecf5
Gordon <1416462572@qq.com>
2024-08-06 18:08:38 +0800

    feat: 完善我的钱包UI

--- lib/ui/wallet/wallet_controller.dart
+++ lib/ui/wallet/wallet_controller.dart
@@ -1,4 +1,5 @@
 import 'package:get/get.dart';
+import 'package:guanjia/ui/wallet/enum/wallet_enum.dart';
 
 import 'wallet_state.dart';
 
@@ -6,4 +7,8 @@ class WalletController extends GetxController {
   final WalletState state = WalletState();
 
   void onTapOrder() {}
+
+  void onTapOperation(WalletOperationType type) {
+    state.typeIndex.value = type;
+  }
 }
---
+++ lib/ui/wallet/wallet_order/wallet_order_controller.dart
@@ -0,0 +1,7 @@
+import 'package:get/get.dart';
+
+import 'wallet_order_state.dart';
+
+class WalletOrderController extends GetxController {
+  final WalletOrderState state = WalletOrderState();
+}

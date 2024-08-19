b68591c573a32794517a4005ba9ef54588c6ce62
Gordon <1416462572@qq.com>
2024-08-19 17:45:30 +0800

    feat: 修改新的注册界面UI，添加密码可见、不可见状态按钮

--- lib/ui/login/widgets/login_text_field.dart
+++ lib/ui/login/widgets/login_text_field.dart
@@ -3,26 +3,29 @@ import 'package:guanjia/common/app_color.dart';
 import 'package:guanjia/common/app_text_style.dart';
 import 'package:guanjia/common/extension/text_style_extension.dart';
 import 'package:guanjia/common/utils/screen_adapt.dart';
+import 'package:guanjia/widgets/app_image.dart';
 
 class LoginTextField extends StatefulWidget {
-  const LoginTextField({
+  LoginTextField({
     super.key,
     this.controller,
-    this.obscureText,
     this.labelText,
     this.hintText,
     this.keyboardType,
-  });
+    this.showPasswordVisible = false,
+  }) : obscureText = showPasswordVisible;
 
   final TextEditingController? controller;
-
-  final bool? obscureText;
 
   final String? labelText;
 
   final String? hintText;
 
   final TextInputType? keyboardType;
+
+  final bool showPasswordVisible;
+
+  bool obscureText;
 
   @override
   State<LoginTextField> createState() => _LoginTextFieldState();
@@ -49,20 +52,42 @@ class _LoginTextFieldState extends State<LoginTextField> {
           borderRadius: BorderRadius.circular(8.rpx),
         ),
         alignment: Alignment.centerLeft,
-        child: TextField(
-          controller: widget.controller,
-          focusNode: focusNode,
-          obscureText: widget.obscureText ?? false,
-          keyboardType: widget.keyboardType,
-          style: AppTextStyle.st.medium.size(14.rpx).textColor(AppColor.black3),
-          decoration: InputDecoration(
-            labelText: widget.labelText,
-            labelStyle: AppTextStyle.st.medium.textColor(AppColor.black92),
-            hintText: widget.hintText,
-            hintStyle: AppTextStyle.st.medium.textColor(AppColor.black92),
-            border: InputBorder.none,
-            isDense: true,
-          ),
+        child: Row(
+          children: [
+            Expanded(
+              child: TextField(
+                controller: widget.controller,
+                focusNode: focusNode,
+                obscureText: widget.obscureText,
+                keyboardType: widget.keyboardType,
+                style: AppTextStyle.st.medium
+                    .size(14.rpx)
+                    .textColor(AppColor.black3),
+                decoration: InputDecoration(
+                  labelText: widget.labelText,
+                  labelStyle:
+                      AppTextStyle.st.medium.textColor(AppColor.black92),
+                  hintText: widget.hintText,
+                  hintStyle: AppTextStyle.st.medium.textColor(AppColor.black92),
+                  border: InputBorder.none,
+                  isDense: true,
+                ),
+              ),
+            ),
+            if (widget.showPasswordVisible)
+              GestureDetector(
+                onTap: () {
+                  widget.obscureText = !widget.obscureText;
+                  setState(() {});
+                },
+                child: AppImage.asset(
+                  widget.obscureText
+                      ? "assets/images/common/password_visible.png"
+                      : "assets/images/common/password_invisible.png",
+                  length: 24.rpx,
+                ),
+              ),
+          ],
         ),
       ),
     );

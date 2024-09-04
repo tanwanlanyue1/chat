582da1884648bb31c4efb465dd3ccb7770c39b4d
wust <1726932926@qq.com>
2024-09-04 19:59:06 +0800

    feat: st修改用户中心轮播标签

---
+++ lib/ui/plaza/user_center/widget/swiper_pagination.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_swiper_null_safety/flutter_swiper_null_safety.dart';
+import 'package:guanjia/widgets/app_image.dart';
+
+class UserSwiperPagination extends SwiperPlugin {
+  ///color when current index,if set null , will be Theme.of(context).primaryColor
+  final Color? activeColor;
+
+  ///,if set null , will be Theme.of(context).scaffoldBackgroundColor
+  final Color? color;
+
+  ///Size of the dot when activate
+  final double activeSize;
+
+  ///Size of the dot
+  final double size;
+
+  /// Space between dots
+  final double space;
+
+  final Key? key;
+
+  UserSwiperPagination(
+      {this.activeColor,
+        this.color,
+        this.key,
+        this.size = 10.0,
+        this.activeSize= 10.0,
+        this.space= 3.0});
+
+  @override
+  Widget build(BuildContext context, SwiperPluginConfig config) {
+    if (config.itemCount > 20) {
+      print("The itemCount is too big, we suggest use FractionPaginationBuilder instead of DotUserSwiperPaginationBuilder in this sitituation");}
+    Color? activeColor = this.activeColor;
+    Color? color = this.color;
+
+    if (activeColor == null || color == null) {
+      ThemeData themeData = Theme.of(context);
+      activeColor = this.activeColor ?? themeData.primaryColor;
+      color = this.color ?? themeData.scaffoldBackgroundColor;
+    }
+
+    if (config.indicatorLayout != PageIndicatorLayout.NONE &&
+        config.layout == SwiperLayout.DEFAULT) {
+      return Container();
+    }
+
+    List<Widget> list = [];
+
+    int itemCount = config.itemCount;
+    int activeIndex = config.activeIndex;
+
+    for (int i = 0; i < itemCount; ++i) {
+      bool active = i == activeIndex;
+      list.add(Container(
+        key: Key("pagination_$i"),
+        margin: EdgeInsets.all(space),
+        child: AppImage.asset(
+          active ? "assets/images/plaza/pagination_select.png" : "assets/images/plaza/pagination.png",
+          width: active ? activeSize : size,
+          height: active ? activeSize : size,
+        ),
+      ));
+    }
+
+    if (config.scrollDirection == Axis.vertical) {
+      return Column(
+        key: key,
+        mainAxisSize: MainAxisSize.min,
+        children: list,
+      );
+    } else {
+      return Row(
+        key: key,
+        mainAxisSize: MainAxisSize.min,
+        children: list,
+      );
+    }
+  }
+}

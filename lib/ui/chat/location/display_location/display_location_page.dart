2f3b1b933514f6b68da92edd722362c51b4d3004
国栋 <xwrite@163.com>
2024-10-23 16:41:05 +0800

    发送，显示 位置

--- lib/ui/chat/location/display_location/display_location_page.dart
+++ lib/ui/chat/location/display_location/display_location_page.dart
@@ -4,9 +4,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:guanjia/common/app_color.dart';
+import 'package:guanjia/common/app_text_style.dart';
 import 'package:guanjia/common/utils/screen_adapt.dart';
 import 'package:guanjia/ui/chat/custom/message_location_content.dart';
 import 'package:google_maps_flutter_platform_interface/google_maps_flutter_platform_interface.dart';
+import 'package:guanjia/widgets/widgets.dart';
 import 'package:image_editor/image_editor.dart';
 
 ///显示消息位置
@@ -17,23 +20,39 @@ class DisplayLocationPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final hasPlace = content.poi.isNotEmpty || content.address.isNotEmpty;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('位置'),
       ),
-      body: Column(
-        children: [Expanded(child: buildMap())],
-      ),
+      body: hasPlace ? Stack(
+        children: [
+          Column(
+            children: [
+              Expanded(child: buildMap()),
+              buildPlace(padding: FEdgeInsets(vertical: 16.rpx)),
+            ],
+          ),
+          Positioned.fill(
+            top: null,
+            bottom: 4.rpx,
+            child: buildPlace(padding: FEdgeInsets(all: 16.rpx)),
+          )
+        ],
+      ) : buildMap(),
     );
   }
 
-  Future<BitmapDescriptor> getMarkerIcon() async{
-    final data = await rootBundle.load('assets/images/chat/ic_chat_location.png');
+  Future<BitmapDescriptor> getMarkerIcon() async {
+    final data =
+        await rootBundle.load('assets/images/chat/ic_chat_location.png');
     final option = ImageEditorOption();
     option.outputFormat = const OutputFormat.png();
     option.addOption(ScaleOption(
       40.px.toInt(),
-      40.px ~/ (160/302),
+      40.px ~/ (160 / 302),
       keepRatio: true,
     ));
     final bytes = await ImageEditor.editImage(
@@ -69,5 +88,34 @@ class DisplayLocationPage extends StatelessWidget {
             rotateGesturesEnabled: false,
           );
         });
+  }
+
+  Widget buildPlace({EdgeInsets? padding}) {
+    return Container(
+      padding: padding,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(8.rpx)),
+        color: Colors.white,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          if (content.poi.isNotEmpty)
+            Text(
+              content.poi,
+              style: AppTextStyle.fs18b.copyWith(
+                color: AppColor.blackBlue,
+              ),
+            ),
+          if (content.address.isNotEmpty)
+            Text(
+              content.address,
+              style: AppTextStyle.fs14m.copyWith(
+                color: AppColor.black3,
+              ),
+            ),
+        ],
+      ),
+    );
   }
 }
